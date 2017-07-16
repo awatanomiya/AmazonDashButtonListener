ce048f0dc63e053b8e44d99aa54a14a96f1bc7c6
Kazuki Awaki <awatanomiya@live.jp>
2017-07-16 17:06:01 +0900

    Add Comments

--- AmazonDashButton/Program.cs
+++ AmazonDashButton/Program.cs
@@ -8,16 +8,31 @@ namespace AmazonDashButton
 {
     class Program
     {
+        /// <summary>
+        /// DashButtonが押されたかを監視するためのリスナ
+        /// </summary>
         static DashButtonListener dashButtonListener;
+
         static void Main(string[] args)
         {
+            /* リスナの初期化
+             * 引数はネットワークの名称(ネットワークと共有センタのアクティブなネットワーク接続 > 接続)
+             */
             dashButtonListener = new DashButtonListener("ワイヤレス ネットワーク接続 4");
-            dashButtonListener.AddDashButton(new byte[] { 252, 166, 103, 164, 220, 166 }).OnPressed += (sender, e) => {
+
+            // DashButtonのMacAdressを設定
+            var dashButton = dashButtonListener.AddDashButton(new byte[] { 252, 166, 103, 164, 220, 166 });
+            
+            // DashButtonが押されたときの動作を定義
+            dashButton.OnPressed += (sender, e) => {
                 Console.WriteLine("ボタンが押されました");
             };
+            
             dashButtonListener.StartCapture();
+
             Console.WriteLine("Press Enter To Stop.");
             Console.ReadLine();
+
             dashButtonListener.StopCapture();
         }
     }
